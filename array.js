a3c788dfa93d5cdcaebad7db090293e00ea97707
Mahbuba Islam <mahbubaislam7010@gmail.com>
2025-12-17 18:16:17 -0500

    problem1-sloved

--- array.js
+++ array.js
@@ -1,3 +1,36 @@
 // **Array.from()**  
 //    - Problem: Convert a string into an array of characters using `Array.from()`.  
-//    - Example Input: `"hello"` → Output: `["h", "e", "l", "l", "o"]`.
+//    - Example Input: `"hello"` → Output: `["h", "e", "l", "l", "o"]`. 
+// if it has any empty string remove this . ex: input - 'This is my book' output - 
+//   ['T', 'h', 'i', 's',
+//   'i', 's', 'm', 'y',
+//   'b', 'o', 'o', 'k'
+// ]
+
+
+
+const charIntoArray = (word) => {
+  const arr = Array.from(word)
+  const results = []
+  for(const word of arr){
+    if(word !== ' '){
+        results.push(word)
+    }
+  }
+ return results
+}
+console.log(charIntoArray('This is my book'));
+
+
+//-------------------------
+
+  // without function
+const results = Array.from('hello')
+console.log(results);
+
+//-------------------------
+
+const char = 'hello'
+const arr = [...char]
+console.log(arr);
+
